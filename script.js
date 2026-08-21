5b118336ae8adec17aff8b1ab982e338a65a9c23
BabariyaRudram <babariyagordhanbhai9@gmail.com>
2026-08-21 17:47:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,7 @@ document.addEventListener("DOMContentLoaded", () => {
       number: "CASE 04",
       year: "1975",
       title: "Sikkim",
-       background: "images/sikkim-flag.svg",
+       background: "images/sikkim-flag.png,
       status: "🔵 FIRST-HAND ACCOUNT",
 
       intro:
